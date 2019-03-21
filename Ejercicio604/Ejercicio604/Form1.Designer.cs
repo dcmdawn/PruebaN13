71610c3fe6e11d238bcf6c2fb3c0a27c7f4da241
Miguel <sanchezpastor19756@iesmarenostrum.com>
2019-03-21 16:27:06 +0100

    Boton 6 añadido

--- Ejercicio604/Ejercicio604/Form1.Designer.cs
+++ Ejercicio604/Ejercicio604/Form1.Designer.cs
@@ -33,13 +33,15 @@
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(35, 13);
+            this.button1.Location = new System.Drawing.Point(26, 11);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(333, 78);
+            this.button1.Size = new System.Drawing.Size(250, 63);
             this.button1.TabIndex = 0;
             this.button1.Text = "Introducir Numeros";
             this.button1.UseVisualStyleBackColor = true;
@@ -47,9 +49,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(35, 97);
+            this.button2.Location = new System.Drawing.Point(26, 79);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(333, 78);
+            this.button2.Size = new System.Drawing.Size(250, 63);
             this.button2.TabIndex = 1;
             this.button2.Text = "Otra lista con primos";
             this.button2.UseVisualStyleBackColor = true;
@@ -57,9 +60,10 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(35, 181);
+            this.button3.Location = new System.Drawing.Point(26, 147);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(333, 78);
+            this.button3.Size = new System.Drawing.Size(250, 63);
             this.button3.TabIndex = 2;
             this.button3.Text = "Quitar a la primera lista los primos y pasarla a otra";
             this.button3.UseVisualStyleBackColor = true;
@@ -67,9 +71,10 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(35, 265);
+            this.button4.Location = new System.Drawing.Point(26, 215);
+            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(333, 78);
+            this.button4.Size = new System.Drawing.Size(250, 63);
             this.button4.TabIndex = 3;
             this.button4.Text = "Mostrar todas las listas";
             this.button4.UseVisualStyleBackColor = true;
@@ -77,23 +82,35 @@
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(35, 349);
+            this.button5.Location = new System.Drawing.Point(26, 284);
+            this.button5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(333, 76);
+            this.button5.Size = new System.Drawing.Size(250, 62);
             this.button5.TabIndex = 4;
             this.button5.Text = "Usseles";
             this.button5.UseVisualStyleBackColor = true;
             // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(26, 352);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(250, 76);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(398, 458);
+            this.ClientSize = new System.Drawing.Size(298, 458);
+            this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -107,6 +124,7 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
     }
 }
 
