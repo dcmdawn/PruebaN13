09845a1ae72e0cdadb5ef1c6f0652cad52f22302
David Carreras Masanet <carreras13223@iesmarenostrum.com>
2019-03-21 16:24:55 +0100

    Boton usseles

--- Ejercicio604/Ejercicio604/Form1.Designer.cs
+++ Ejercicio604/Ejercicio604/Form1.Designer.cs
@@ -32,6 +32,7 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -74,11 +75,21 @@
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(35, 349);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(333, 76);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Usseles";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(398, 356);
+            this.ClientSize = new System.Drawing.Size(398, 458);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -95,6 +106,7 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
     }
 }
 
